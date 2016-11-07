1ccc936ef70cbeeb8d125623624b0d6a3ddefd5e
PINEL <PINEL@s155p02.bts.org>
2016-11-07 13:50:30 +0100

    Premier commit

--- public_html/index.php
+++ public_html/index.php
@@ -6,11 +6,11 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>SALUT !</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>TODO write content</div>
+        <div><h4>Un berlingot de lait.</h4></div>
     </body>
 </html>
